03fcce940b83bf75999d3f2bcec40606a9493c27
saptarshi1996 <saptarshisg96@gmail.com>
2021-12-18 05:49:52 +0530

    nuxt client added

---
+++ client/layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app dark>
+    <v-app-bar :clipped-left="clipped" fixed app>
+      <v-toolbar-title v-text="title" />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "DefaultLayout",
+  data() {
+    return {
+      clipped: false,
+      title: "Walvis",
+    };
+  },
+};
+</script>
---
+++ client/pages/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="4">
+      <v-card class="mx-auto" max-width="300" tile>
+        <v-list dense>
+          <v-subheader>CONTAINERS</v-subheader>
+          <v-list-item-group v-model="selectedItem" color="error">
+            <v-list-item v-for="(item, i) in containerList" :key="i" @click.prevent="getLogs(item.id)">
+              <v-list-item-content>
+                <v-list-item-title v-text="item.name"></v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-card>
+    </v-col>
+    <v-col cols="8" sm="8" md="6">
+      <v-card></v-card>
+    </v-col>
+  </v-row>
+</template>
+
+
+<script>
+
+import Vue from "vue";
+import VueSSE from 'vue-sse';
+
+// using defaults
+Vue.use(VueSSE);
+
+export default {
+
+  name: "IndexPage",
+
+  data() {
+    return {
+      containerList: [],
+      selectedItem: null,
+    };
+  },
+
+  async created() {
+    await this.getContainerList();
+  },
+
+  methods: {
+    async getContainerList() {
+      const { data } = await this.$axios.get("/list");
+      this.containerList = data.Response.data;
+    },
+
+    getLogs(id) {
+      this.$sse.create(`http://localhost:9999/stream/${id}`)
+        .on('message', (msg) => console.info('Message:', msg))
+        .on('error', (err) => console.error('Failed to parse or lost connection:', err))
+        .connect()
+        .catch((err) => console.error('Failed make initial connection:', err));
+    },
+
+  },
+};
+
+</script>
