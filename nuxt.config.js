53a004d6616b9a352f49d91f8d01540dc606dbab
saptarshi1996 <saptarshisg96@gmail.com>
2021-12-23 02:33:15 +0530

    nuxt custom link

--- nuxt.config.js
+++ nuxt.config.js
@@ -53,7 +53,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://localhost:9999',
+    baseURL: 'http://0.0.0.0:9999',
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
