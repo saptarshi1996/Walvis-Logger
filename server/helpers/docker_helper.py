5d3c7c5332b9b84517f45675a8edb36cf15d33ba
saptarshi1996 <saptarshisg96@gmail.com>
2022-02-01 10:29:25 +0530

    format py

--- server/helpers/docker_helper.py
+++ server/helpers/docker_helper.py
@@ -2,40 +2,36 @@ from posixpath import split
 import docker
 
 
-def get_client():
+def get_client() -> docker.DockerClient:
 
     """
-        Get the client by socket
+    Get the client by socket
     """
 
     return docker.DockerClient(base_url="unix://var/run/docker.sock")
 
 
-def list_containers():
+def list_containers() -> list:
 
     """
-        List all containers by name and id
+    List all containers by name and id
     """
 
     try:
         containers = get_client().containers.list()
         container_list = []
         for container in containers:
-            container_list.append({
-                "id": container.id,
-                "name": container.name,
-                "status": container.status
-            })
+            container_list.append(
+                {"id": container.id, "name": container.name, "status": container.status}
+            )
 
         return container_list
 
-    except Exception as e:
-
-        print(e)
+    except (ValueError, KeyError):
         return []
 
 
-def split_by_date(log):
+def split_by_date(log) -> dict:
 
     """[summary]
     Split time and messge of docker container by date.
@@ -48,8 +44,8 @@ def split_by_date(log):
     message = " ".join(message_list)
     if message:
 
-        date, time = time.split('T')
-        date_time = date+" "+time[0:8]
+        date, time = time.split("T")
+        date_time = date + " " + time[0:8]
 
         return {
             "timestamp": date_time,
@@ -57,21 +53,24 @@ def split_by_date(log):
         }
 
 
-def download_logs(id):
+def download_logs(id) -> dict:
 
     """[summary]
     Download docker logs and convert them to json or list
+
+    [args]
+    [int] id of container
 
     Returns:
         [type]: [description]
     """
 
-    logs = get_client().containers.get(id).logs(timestamps=True) 
+    logs = get_client().containers.get(id).logs(timestamps=True)
     log_list = logs.decode("utf-8").split("\n")
     log_array = list(map(split_by_date, log_list))
 
     if len(log_array) == 0:
-        return { "logs": [] }
-    
-    log_array = filter(lambda x : x, log_array)
-    return { "logs": list(log_array) }
+        return {"logs": []}
+
+    log_array = filter(lambda x: x, log_array)
+    return {"logs": list(log_array)}
--- server/main.py
+++ server/main.py
@@ -15,54 +15,57 @@ cors = CORS(app)
 ################################  CONTROLLERS ##########################################
 
 
-@app.route('/logger-server/container_list', methods=["GET"])
+@app.route("/logger-server/container_list", methods=["GET"])
 @cross_origin()
 def get_container_list():
     container_list = docker_helper.list_containers()
     return jsonify(Response={"data": container_list}), 200
 
 
-@app.route('/logger-server/stream/<id>')
+@app.route("/logger-server/stream/<id>")
 @cross_origin()
 def stream_logs(id):
 
     # check if the container is running, else don't run the stream
-    tail = request.args.get('tail')
+    tail = request.args.get("tail")
 
     if tail and tail != "all":
         tail = int(tail)
 
-    if request.headers.get('accept') == 'text/event-stream':
+    if request.headers.get("accept") == "text/event-stream":
+
         def events():
 
             container = docker_helper.get_client().containers.get(id)
-            target = container.logs(stream=True, follow=True, tail=tail, timestamps=True)
+            target = container.logs(
+                stream=True, follow=True, tail=tail, timestamps=True
+            )
 
             try:
                 while True:
 
                     line = next(target)
-                    
+
                     timestamp, *message_list = line.decode("utf-8").split(" ")
                     message = " ".join(message_list)
 
-                    log = { "timestamp": timestamp, "message": message }
+                    log = {"timestamp": timestamp, "message": message}
 
                     yield "data: %s\n\n" % (json.dumps(log))
                     time.sleep(0.1)  # an artificial delay
             except StopIteration:
                 print("Logger stopped")
 
-        return Response(events(), mimetype='text/event-stream')
+        return Response(events(), mimetype="text/event-stream")
 
     return Response(status=400)
 
 
-@app.route('/logger-server/stats/<id>')
+@app.route("/logger-server/stats/<id>")
 @cross_origin()
 def stream_stats(id):
 
-    if request.headers.get("accept") == 'text/event-stream':
+    if request.headers.get("accept") == "text/event-stream":
 
         def events():
 
@@ -76,9 +79,9 @@ def stream_stats(id):
                     time.sleep(0.1)  # an artificial delay
             except StopIteration:
                 print("Logger stopped")
-        
-        return Response(events(), mimetype='text/event-stream')
-    
+
+        return Response(events(), mimetype="text/event-stream")
+
     return Response(status=400)
 
 
@@ -87,9 +90,11 @@ def stream_stats(id):
 def download_logs(id):
     try:
 
-        format, file_name = request.args.get('log_format'), request.args.get('file_name')
+        format, file_name = request.args.get("log_format"), request.args.get(
+            "file_name"
+        )
 
-        if format == 'json':
+        if format == "json":
 
             file_object = docker_helper.download_logs(id)
 
@@ -98,15 +103,20 @@ def download_logs(id):
             buffer.write(bytes(json_string, encoding="utf-8"))
             buffer.seek(0)
 
-            return send_file(buffer, as_attachment=True, attachment_filename="{0}.json".format(file_name.replace(" ", "")), mimetype="application/json")
+            return send_file(
+                buffer,
+                as_attachment=True,
+                attachment_filename="{0}.json".format(file_name.replace(" ", "")),
+                mimetype="application/json",
+            )
 
-        if format == 'csv':
+        elif format == "csv":
 
             file_object = docker_helper.download_logs(id)
             string_buffer = StringIO()
 
-            logs = file_object['logs']
-            log_list = [[log['timestamp'], log['message']] for log in logs]
+            logs = file_object["logs"]
+            log_list = [[log["timestamp"], log["message"]] for log in logs]
 
             writer = csv.writer(string_buffer)
             writer.writerows(log_list)
@@ -118,7 +128,12 @@ def download_logs(id):
             string_buffer.close()
 
             # send file to user end
-            return send_file(buffer, as_attachment=True, attachment_filename="{0}.csv".format(file_name.replace(" ", "")), mimetype="text/csv")
+            return send_file(
+                buffer,
+                as_attachment=True,
+                attachment_filename="{0}.csv".format(file_name.replace(" ", "")),
+                mimetype="text/csv",
+            )
 
     except Exception as e:
         print(e)
@@ -129,4 +144,4 @@ def download_logs(id):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=9099)
+    app.run(host="0.0.0.0", port=9099)
