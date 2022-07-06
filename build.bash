8f03e496f3599832f1a4ddaff9ce96bc4fd7504b
Saptarshi <saptarshisg96@gmail.com>
2022-07-06 16:58:16 +0530

    docker build remove dangling

--- build.bash
+++ build.bash
@@ -1,2 +1,3 @@
+docker rmi $(sudo docker images -f "dangling=true" -q) --force
 docker build -t saptarshi/walvis-logger .
-docker run -p 8000:8000 -d saptarshi/walvis-logger
+docker run -p 8000:8000 -v /var/run/docker.sock:/var/run/docker.sock -d saptarshi/walvis-logger
