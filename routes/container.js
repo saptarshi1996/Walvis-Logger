fa18cc083a68881e704a2a0f0a53ee1f0f77f4da
saptarshi1996 <saptarshisg96@gmail.com>
2022-07-06 21:03:38 +0530

    remove comment

--- routes/container.js
+++ routes/container.js
@@ -4,6 +4,5 @@ const containerController = require('../controllers/container');
 
 router.get('/listContainers', containerController.listContainers);
 router.get('/containerDetails/:id', containerController.getContainerDetails);
-router.get('/logs', containerController.getContainerLogs);
 
 module.exports = router;
