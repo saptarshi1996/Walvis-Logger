d00997d58708631957a34c38665c085a3f502266
saptarshi1996 <saptarshisg96@gmail.com>
2022-07-07 00:50:09 +0530

    remove sudo from build

--- build.bash
+++ build.bash
@@ -1,3 +1,3 @@
-docker rmi $(sudo docker images -f "dangling=true" -q) --force
+docker rmi $(docker images -f "dangling=true" -q) --force
 docker build -t saptarshi/walvis-logger .
 docker run -p 8000:8000 -v /var/run/docker.sock:/var/run/docker.sock -d saptarshi/walvis-logger
