00fa75dbddfb38af51591e536aa741d10e0cd0c0
saptarshi1996 <saptarshisg96@gmail.com>
2022-07-10 01:33:07 +0530

    remove extra lines

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,11 +1,9 @@
 <template>
   <v-row> 
     <v-col cols="12">
-
       <div class="d-flex justify-center mt-6" v-if="!loadingLogs && logs && logs.length === 0">
         <h2>Select a container to view logs</h2>
       </div>
-
       <v-alert dense class="mb-0 ml-1" v-for="(log, i) in logs" :key="i">
         <v-chip label class="mr-2">{{ log.timeStamp }}</v-chip>{{ log.log }}
       </v-alert>
