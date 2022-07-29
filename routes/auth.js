36a540d2ac3d80570c2aac6cc945a97514c5930a
saptarshi1996 <saptarshisg96@gmail.com>
2022-07-30 01:15:19 +0530

    start from 2.0

--- routes/auth.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-
-const authController = require('../controllers/auth');
-
-router.post('/userLogin', authController.userLogin);
-router.get('/userDetails', authController.userDetails);
-
-module.exports = router;
