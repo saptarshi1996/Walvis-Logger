2cff0131341bfaea1021eba59289f3bbb56c3770
saptarshi1996 <saptarshisg96@gmail.com>
2022-08-20 22:09:18 +0530

    break lines in css

--- client/components/Loader.vue
+++ client/components/Loader.vue
@@ -11,6 +11,7 @@
   width: 80px;
   height: 80px;
 }
+
 .lds-dual-ring-white:after {
   content: " ";
   display: block;
@@ -22,6 +23,7 @@
   border-color: #fff transparent #fff transparent;
   animation: lds-dual-ring-white 1.2s linear infinite;
 }
+
 @keyframes lds-dual-ring-white {
   0% {
     transform: rotate(0deg);
@@ -30,11 +32,13 @@
     transform: rotate(360deg);
   }
 }
+
 .lds-dual-ring-black {
   display: inline-block;
   width: 80px;
   height: 80px;
 }
+
 .lds-dual-ring-black:after {
   content: " ";
   display: block;
@@ -46,6 +50,7 @@
   border-color: #000 transparent #000 transparent;
   animation: lds-dual-ring-black 1.2s linear infinite;
 }
+
 @keyframes lds-dual-ring-black {
   0% {
     transform: rotate(0deg);
